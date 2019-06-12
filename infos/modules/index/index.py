12488673abf88d1f6f730e317a43c68c410449fc
IzayoiRin <1204149237@qq.com>
2019-06-12 09:14:20 +0800

    News set login state(component) remove debug info

--- infos/modules/index/index.py
+++ infos/modules/index/index.py
@@ -12,13 +12,10 @@ class IndexLogical(object):
 
     def login_state(self):
         uid = self.requset
-        print(uid)
         if not uid:
             self.response = json.jsonify(errno=rc.RET.SESSIONERR, errmsg="Session Invalid")
         user = Users.packQuery(Users.id == uid)
-        print(user[0])
         select_user = user[0] if user else None
-        print(select_user)
         if select_user:
             self.response = json.jsonify(errno=rc.RET.OK, name=select_user.nick_name, url=select_user.avatar_url)
             return
